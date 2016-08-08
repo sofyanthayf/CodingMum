d873e09f2c8c9fe9d99e88c935bd4f7d184e782d
Sofyan Thayf <sofyanthayf@gmail.com>
2016-08-08 14:31:21 +0800

    update #8 dan #9
    Catatan dan contoh kasus sesi pertemuan #8 dan #9

--- Latihan4/js/codingmum.js
+++ Latihan4/js/codingmum.js
@@ -9,12 +9,35 @@
 	Menghitung Jumlah Harga dan Discount
 	- Harga satuan fixed 2500
 	- Discout fixed 10% (0.1)
+
+	@args:  jumlah = jumlah pembelian
 */
 function hitungJumlahHarga( jumlah ) {
 	var hasil;
 	var total;
 	total = jumlah * 2500;
 	hasil = total - (total * 0.1);
+
+	return hasil;
+}
+
+/*
+	Menghitung Jumlah Harga dan Discount
+	- Harga satuan fixed 2500
+	- Discout 10% (0.1) untuk total pembelian lebih dari 25000 
+
+	@args:  jumlah = jumlah pembelian
+*/
+function hitungJumlahHargaDiscountBersyarat( jumlah ){
+	var hasil;
+	var total;
+	total = jumlah * 2500;
+
+	if ( total >= 25000 ) {
+		hasil = total - (total * 0.1);  
+	} else {
+		hasil = total;
+	}
 
 	return hasil;
 }
