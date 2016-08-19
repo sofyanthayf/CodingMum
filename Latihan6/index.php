3f9534a47c52db38ac30b9b1a8e7023d3acfb5f9
Sofyan Thayf <sofyanthayf@gmail.com>
2016-08-19 09:10:01 +0800

    Update #12
    Latihan 6 dari pertemuan #12  --> database

---
+++ Latihan6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First PHP</title>
+</head>
+<body>
+
+	<?php
+		echo "<h1>Hello, World</h1>";
+	?>
+
+</body>
+</html>
---
+++ Latihan6/registeruser.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrasi User</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/formreg.css">
+</head>
+<body>
+
+<div class="container">
+    <h1 class="well">Registration Form</h1>
+	<div class="col-lg-12 well">
+	<div class="row">
+				<form>
+					<div class="col-sm-12">
+					<div class="form-group">
+						<label>Nama Anda</label>
+						<input type="text" placeholder="Nama Anda.." class="form-control">
+					</div>		
+					<div class="form-group">
+						<label>Alamat Email</label>
+						<input type="text" placeholder="Email Anda.." class="form-control">
+					</div>	
+					<div class="form-group">
+						<label>Alamat Rumah</label>
+						<input type="text" placeholder="Alamat rumah Anda.." class="form-control">
+					</div>	
+					<div class="form-group">
+						<label>Kota</label>
+						<input type="text" placeholder="Kota Anda.." class="form-control">
+					</div>	
+					<div class="form-group">
+						<label>Nomor Telepon</label>
+						<input type="text" placeholder="Nomor telepon/hp.." class="form-control">
+					</div>		
+					<div class="form-group">
+						<label>Password</label>
+						<input type="text" placeholder="Password Anda.." class="form-control">
+					</div>		
+					<button type="button" class="btn btn-lg btn-info">Submit</button>					
+					</div>
+				</form> 
+				</div>
+	</div>
+	</div>
+
+
+
+</body>
+</html>
