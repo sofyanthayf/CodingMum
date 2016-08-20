45ba5903621108f4ebcaccb95643988a0b5f4a9a
Sofyan Thayf <sofyanthayf@gmail.com>
2016-08-20 09:09:37 +0800

    Perbaikan tampilan
    Perbaikan tampilan halaman index.php dan regsukses.php , dengan
    penambahan style pada <DIV>
    <DIV class="container"> dan <DIV class="well">
    
    class "well" adalah style bawaan css/formreg.css

--- Latihan6/index.php
+++ Latihan6/index.php
@@ -2,13 +2,19 @@
 <html>
 <head>
 	<title>My First PHP</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="css/formreg.css">
 </head>
 <body>
+<div class="container">
+	<div class="well">
 
 	<?php
 		echo "<h1>Hello, World</h1>";
 	?>
 
+	</div>
+</div>
 </body>
 </html>
--- Latihan6/regsukses.php
+++ Latihan6/regsukses.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title></title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="css/formreg.css">
 </head>
+
 <body>
-<h2>Selamat, Registrasi Sukses</h2>
+
+<div class="container">
+	<div class="well">
+
+	<h2>Selamat, Registrasi Sukses</h2>
+
+	<h4>Silahkan <a href="index.php">kembali ke halaman depan</a></h4>
+
+	</div>
+</div>
+
 </body>
 </html>
